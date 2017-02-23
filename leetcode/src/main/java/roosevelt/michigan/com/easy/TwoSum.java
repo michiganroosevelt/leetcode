7bf16c9bdee1aa09c7e65cc06c860e2167c166b4
michigan.roosevelt@gmail.com <michigan.roosevelt@gmail.com>
2017-02-22 20:07:33 -0800

    started TwoSum

---
+++ leetcode/src/main/java/roosevelt/michigan/com/easy/TwoSum.java
@@ -0,0 +1,12 @@
+package roosevelt.michigan.com.easy;
+
+public class TwoSum {
+	
+	
+	public int[] twoSum(int[] nums, int target) {
+		
+		
+		return null;
+	}
+
+}
---
+++ leetcode/src/test/java/roosevelt/michigan/com/easy/TwoSumTest.java
@@ -0,0 +1,14 @@
+package roosevelt.michigan.com.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TwoSumTest {
+
+	
+	@Test
+	public void testTwoSum() {
+		Assert.assertEquals(true, true);
+	}
+	
+}
